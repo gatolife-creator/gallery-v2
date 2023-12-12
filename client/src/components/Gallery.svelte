f29989ce531c68d43cdef79988467f3bc3a593d0
がとらいふ <78526558+gatolife-creater@users.noreply.github.com>
2023-12-12 22:42:32 +0900

    add: upload API

--- client/src/components/Gallery.svelte
+++ client/src/components/Gallery.svelte
@@ -1,13 +1,49 @@
 <script lang="ts">
   import { onMount } from "svelte";
   import PhotoSwipeLightbox from "photoswipe/lightbox";
+  import Dropzone from "svelte-file-dropzone/Dropzone.svelte";
   import "photoswipe/style.css";
 
   import type { Image } from "../types";
 
-  let images: Image[] = [];
+  // ドラッグアンドドロップが終了したら自動的にアップロードを実行する
+  function handleDrop(_: DragEvent, files: File[]) {
+    handleFileUpload(files);
+  }
 
-  onMount(async () => {
+  let images: Image[] = [];
+  let uploaded = false;
+
+  async function handleFileUpload(files: File[]) {
+    const formData = new FormData();
+    console.log(files);
+    files.forEach((file: File) => {
+      formData.append("image", file);
+    });
+
+    try {
+      const response = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      // レスポンスをJSON形式で取得
+      const result = await response.json();
+
+      if (response.ok) {
+        console.log(result.message); // 成功メッセージを表示
+      } else {
+        console.error(result.message); // エラーメッセージを表示
+      }
+    } catch (error) {
+      console.error("アップロード中にエラーが発生しました。", error);
+    }
+
+    uploaded = true;
+    await getImages();
+  }
+
+  async function getImages() {
     const res = await fetch("api/images");
     images = await res.json();
 
@@ -17,8 +53,21 @@
       pswpModule: () => import("photoswipe"),
     });
     lightbox.init();
+  }
+
+  onMount(async () => {
+    await getImages();
   });
 </script>
+
+<Dropzone
+  accept="image/*"
+  inputElement={null}
+  on:drop={(e) => {
+    console.log(e);
+    handleDrop(e.detail, e.detail.acceptedFiles);
+  }}
+/>
 
 <div class="pswp-gallery masonry-gallery" id="test">
   {#each images as image (image.src)}
--- server/api.ts
+++ server/api.ts
@@ -2,10 +2,62 @@ import { Router } from "express";
 import path from "path";
 import fs from "fs";
 import sizeOf from "image-size";
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (_1, _2, cb) => {
+    cb(null, "images/"); // 画像の保存先ディレクトリ
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 export const router = Router();
 
-router.get("/api/images", (req, res) => {
+router.get("/api/images", (_, res) => {
+  const imagesDir = path.join(__dirname, "images");
+
+  const imageExtensions = [
+    ".jpg",
+    ".jpeg",
+    ".png",
+    ".gif",
+    ".bmp",
+    ".tif",
+    ".tiff",
+    ".webp",
+  ];
+
+  fs.readdir(imagesDir, (err, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Server Error");
+    } else {
+      const images = files
+        .filter((file) => {
+          const ext = path.extname(file).toLowerCase();
+          return imageExtensions.includes(ext);
+        })
+        .map((file) => {
+          const src = `/images/${file}`;
+          const dimensions = sizeOf(path.join(imagesDir, file));
+
+          return {
+            src,
+            width: dimensions.width,
+            height: dimensions.height,
+          };
+        });
+
+      res.json(images);
+    }
+  });
+});
+
+router.get("/api/images", (_, res) => {
   const imagesDir = path.join(__dirname, "images");
 
   fs.readdir(imagesDir, (err, files) => {
@@ -27,4 +79,16 @@ router.get("/api/images", (req, res) => {
       res.json(images);
     }
   });
+});
+
+router.post("/api/upload", upload.array("image"), (req, res) => {
+  console.log(req.files);
+  try {
+    res.send({ message: "画像がアップロードされました。" });
+  } catch (error) {
+    res.status(400).send({
+      message: "画像のアップロード中にエラーが発生しました。",
+      error: error,
+    });
+  }
 });
